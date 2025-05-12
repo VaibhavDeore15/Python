b6d0bc73c066aead5eb174fe0446c8189e263a12
VaibhavDeore15 <145250142+VaibhavDeore15@users.noreply.github.com>
2025-05-12 21:10:49 +0530

    Add files via upload

---
+++ Basic Python/Basic/ass5.py
@@ -0,0 +1,6 @@
+# WAP to check whether The entered number is divisible by 5 or not
+num=int(input("Enter number "))
+if num%5==0:
+    print(num, "is divisible by 5")
+else:
+    print(num, "is not divisible by 5")
